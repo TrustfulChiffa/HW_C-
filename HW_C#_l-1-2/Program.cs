13989b35360e689764c5331340ac43f4591de2bc
Chiffa <babiy99021422@gmail.com>
2022-12-19 22:55:37 +0300

    Сделали Задание 2

--- HW_C#_l-1-2/Program.cs
+++ HW_C#_l-1-2/Program.cs
@@ -4,3 +4,26 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if (num1 > num2) 
+{
+    max = num1;
+    min = num2;
+}
+
+else 
+{
+    max = num2;
+    min = num1;
+}
+
+Console.WriteLine($"{max} больше {min}");
