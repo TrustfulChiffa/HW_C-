8d3b0fcde9f891db5a41133f88e3b74b44edcbf5
Chiffa <babiy99021422@gmail.com>
2022-12-19 23:19:01 +0300

    Выполнили задание 6

--- HW_C#_l-1-2/Program.cs
+++ HW_C#_l-1-2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Branch task_6
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0) 
+Console.WriteLine($"{num} -> да");
+
+else Console.WriteLine($"{num} -> нет");
