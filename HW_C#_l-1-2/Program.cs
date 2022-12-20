35e705d0c92c50dfcc7c7d77c4707ebe0f2e2f55
Chiffa <babiy99021422@gmail.com>
2022-12-20 20:38:13 +0300

    Сделали выдачу в терминале задания 2 более лаконичной

--- HW_C#_l-1-2/Program.cs
+++ HW_C#_l-1-2/Program.cs
@@ -5,11 +5,15 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.Clear();
+
 Console.Write("Введите первое число ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе число ");
 int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
 
 int max = 0;
 int min = 0;
