826c440d44bc5348e83a1a837edc0ad58aacf44f
Chiffa <babiy99021422@gmail.com>
2022-12-21 00:27:12 +0300

    Сделал задание 13

--- HW_C#_l-1-2/Program.cs
+++ HW_C#_l-1-2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Branch task_13
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+if (N < 100) Console.Write($"{N} -> третьей цифры нет");
+else
+{
+    int i = N;
+    while (i > 999)
+    {
+        i /= 10;
+    }
+    Console.Write($"{N} -> {i % 10}");
+
+}
