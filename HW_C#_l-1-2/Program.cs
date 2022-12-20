d0b4355338b3238449b9679df29fa2fdf3535358
Chiffa <babiy99021422@gmail.com>
2022-12-20 20:34:54 +0300

    Сделали задание 8

--- HW_C#_l-1-2/Program.cs
+++ HW_C#_l-1-2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Branch task_8
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+int count = 2;
+
+Console.Write($"{N} -> ");
+
+if(N % 2 == 0)
+{
+    while (count <= N-2)
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    }
+    Console.Write(N);
+}
+
+else
+{
+    while (count <= N-3)
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    }
+    Console.Write(N-1);
+}
